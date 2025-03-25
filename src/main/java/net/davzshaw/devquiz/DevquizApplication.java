fe0a47d54a4a5dfb4c6857aae41b86fd69ec57d3
davzshaw <juandis0246@gmail.com>
2025-03-25 18:23:24 -0500

    init commit

---
+++ src/main/java/net/davzshaw/devquiz/DevquizApplication.java
@@ -0,0 +1,14 @@
+package net.davzshaw.devquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevquizApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		SpringApplication.run(DevquizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/davzshaw/devquiz/DevquizApplicationTests.java
@@ -0,0 +1,13 @@
+package net.davzshaw.devquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
